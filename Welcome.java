1c8a1b275a849add863ad74d1c5be42793fb983b
Kirti-321 <74565216+Kirti-321@users.noreply.github.com>
2020-11-20 10:40:11 +0530

    Topics to learn comitted

--- Welcome.java
+++ Welcome.java
@@ -3,5 +3,10 @@ public class Welcome
     public static void main( String[] args )
     {
        System.out.println("Welcome to java tutorial");
+        System.out.println("We will learn the following things in this tutorial");
+        System.out.println("Basic Syntax");
+        System.out.println("Operators");
+        System.out.println("if-else Statement");
+        System.out.println("switch-case Statement");
     }
 }
