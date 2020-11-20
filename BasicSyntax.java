da9615999070b4560cc4333e1f29da4886f7cab6
Kirti-321 <74565216+Kirti-321@users.noreply.github.com>
2020-11-20 11:00:27 +0530

    BasicSyntaxFinalCommit

--- BasicSyntax.java
+++ BasicSyntax.java
@@ -2,7 +2,8 @@ public class BasicSyntax
 {
     public static void main( String[] args )
     {
-        System.out.println("Every main class will be enclosed inside a public class")
+        System.out.println("Every main class will be enclosed inside a public class");
         System.out.println("'public static void main( String[] args )' is mandatory syntax for the main method");
+        System.out.println("Done with basic syntax");
     }
 }
